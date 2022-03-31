3adb7da1b151842c5b354b2f343f7a096a3c5943
Laureent <asdasd@gmail.com>
2022-03-31 14:02:03 +0200

    card

--- plant/src/App.vue
+++ plant/src/App.vue
@@ -14,9 +14,28 @@ export default{
 </script>
 
 <template>
-  <ul>
-    <li v-for="noveny in novenyek" :key="noveny.id">{{noveny.tipus}} {{noveny.szin}}</li>
-  </ul>
+    <div id="app">
+    <v-app id="inspire">
+      <v-card :loading="loading" class="mx-auto my-12" max-width="374">
+
+        <v-img height="250" v-for="noveny in novenyek" :key="noveny.id" srv="{{noveny.kep}}"></v-img>
+    
+        <v-card-title v-for="noveny in novenyek" :key="noveny.id">{{noveny.tipus}}</v-card-title>
+    
+        <v-card-text>
+          <v-row align="center" class="mx-0">
+          <div v-for="noveny in novenyek" :key="noveny.id">{{noveny.leiras}}</div>
+          </v-row>
+        </v-card-text>
+    
+        <v-divider class="mx-4"></v-divider>
+    
+        <v-card-actions>
+          <v-btn color="deep-purple lighten-2" text>Módosítás</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-app>
+  </div>
 </template>
 
 <style>
